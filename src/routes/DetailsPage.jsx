4f917000adc359d21d66e09daa1188fcb284c1f1
Matt <matthaynes412@gmail.com>
2023-04-11 02:38:01 -0400

    modify styling color palette

--- src/routes/DetailsPage.jsx
+++ src/routes/DetailsPage.jsx
@@ -2,10 +2,16 @@ import { useEffect, useState } from 'react';
 import AngryBirdTransparent from '../assets/angry-bird-transparent.webp';
 import { useParams } from "react-router-dom";
 import { supabase } from '../client'
+import NavBar from '../components/NavBar';
 
 const DetailsPage = () => {
 
-    const [post, setPost] = useState({})
+    const [post, setPost] = useState([{
+        id:1,
+        name:"Robin",
+        double_damage:"Brick",
+        color:"Red"
+    }])
     
     let id = useParams().id;
     console.log(post)
@@ -25,9 +31,10 @@ const DetailsPage = () => {
 
     return (
         <div className='DetailsPage'>
+            <NavBar />
             <img src={AngryBirdTransparent} alt="Angry Bird" width="350px"/>
             <h2>This is "{post[0].name}", a {post[0].color} Bird unique of its kind.</h2>
-            <h2> Use this bird to deal double damage to {post[0].double_damage} Walls!</h2>
+            <h2>Use this bird to deal double damage to {post[0].double_damage} Walls!</h2>
         </div>
     );
   };
